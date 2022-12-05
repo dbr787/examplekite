301f86aacaee74fdad2c756d82dc7d7721b7c34f
Daniel Ring <dbr787@gmail.com>
2022-12-05 23:40:00 +1100

    first real commit

---
+++ aws/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -exo pipefail
+
+# # create pre-checkout hook
+# cat <<EOF >>/etc/buildkite-agent/hooks/pre-checkout
+# #!/bin/bash
+# set -eo pipefail
+# # Created on $(date)
+# echo "--- :evergreen_tree: Pre-Checkout Hook"
+# env
+# echo "pre-checkout hook executed successfully"
+# # buildkite-agent annotate "pre-checkout hook executed successfully 🚀"
+# EOF
+
+# # update permissions on pre-checkout hook
+# sudo chmod +x /etc/buildkite-agent/hooks/pre-checkout
